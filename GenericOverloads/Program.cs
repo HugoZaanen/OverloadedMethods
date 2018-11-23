ade9e4c216ca3095486fca4b398777b1d9c71fa4
Huego <hugozaanen@hotmail.com>
2018-11-23 19:57:23 +0100

    Add project files.

---
+++ GenericOverloads/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericOverloads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
